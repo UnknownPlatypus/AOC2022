dd5ffd82d609647041baafc1e2ee1a6798140168
Thibaut Decombe <thibaut.decombe@gmail.com>
2022-12-07 00:46:20 +0100

    Add day1

---
+++ day01/part2.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+points = {"A": 1, "B": 2, "C": 3}
+win = {"C": "B", "B": "A", "A": "C"}
+
+
+def compute(s: str) -> int:
+    totals = sorted(
+        sum(int(line) for line in chunk.splitlines()) for chunk in s.split("\n\n")
+    )[-3:]
+    return sum(totals[-3:])
+
+
+INPUT_S = """\
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+"""
+EXPECTED = 45000
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
